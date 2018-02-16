24b47d9f6a9ea1d62c48302ae1adc1752165ab63
Pritoj Singh <pritojs@gmail.com>
2018-02-16 22:40:47 +0530

    Added Actions.js properly

--- src/Action.js
+++ src/Action.js
@@ -10,30 +10,47 @@ class Action {
   constructor(name) {
     // assign name to this action
     this.name = name;
+    
+    // set a default description, hopefully will be added later
+    this.description = `This action is the ${name} action. Much amaze`;
     // set to a default response
     this.response = defaultResponse;
+    
     this.availability = (action, engine, character)=>(true);
   }
   
-  /**
-   * This method sets the name of the action.
-   * THis metod is chainable so it returns this object.
-   */
+ /**
+  * This method sets the name of the action.
+  * This metod is chainable so it returns this object.
+  * @param {string} name 
+  */
   setActionName(name){
     this.name = name;
     return this;
   }
 
   /**
+   * This sets the action description.
+   * @param {string} desc 
+   */
+  setActionDescription(desc){
+    this.description = desc;
+  }
+
+  /**
    * This method helps define a response to an action.
    * By default this function is passed the action, the engine and the charachter
+   * @param {function} resp 
    */
   setActionResponse(resp){
     this.response = resp;
   }
 
-  // This is used to check if this action is available
-  // at this juncture.
+  /**
+   * This methods verifies if this action will be available to the player.
+   * It is passed the action, engine and character in that order.
+   * @param {function} availability 
+   */
   setAvailablity(availability){
     // set the availability to the function passed so it'll be checked
     this.availability = availability;
